8d2a70f50af29d8498e9c093ccb177da4bffcc6b
Bram de Jong <bram.dejong@native-instruments.de>
2015-02-22 18:40:18 +0100

    added installer script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -sf `pwd`/.bash_profile ~/.bash_profile
+ln -sf `pwd`/.bash_prompt ~/.bash_prompt
+ln -sf `pwd`/.brewpackages ~/.brewpackages
+ln -sf `pwd`/.gitconfig ~/.gitconfig
+ln -sf `pwd`/.gitignore_global ~/.gitignore_global
+ln -sf `pwd`/.vimrc ~/.vimrc
+
