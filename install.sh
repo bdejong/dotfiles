4f97e11fc7d02e686b432c0605ccd72a15205a5a
Bram de Jong <bram.dejong@native-instruments.de>
2015-03-09 09:00:23 +0100

    Improved install script

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-ln -sf `pwd`/.bash_profile ~/.bash_profile
-ln -sf `pwd`/.bash_prompt ~/.bash_prompt
-ln -sf `pwd`/.brewpackages ~/.brewpackages
-ln -sf `pwd`/.gitconfig ~/.gitconfig
-ln -sf `pwd`/.gitignore_global ~/.gitignore_global
-ln -sf `pwd`/.vimrc ~/.vimrc
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+ln -sf $DIR/.bash_profile ~/.bash_profile
+ln -sf $DIR/.bash_prompt ~/.bash_prompt
+ln -sf $DIR/.brewpackages ~/.brewpackages
+ln -sf $DIR/.gitconfig ~/.gitconfig
+ln -sf $DIR/.gitignore_global ~/.gitignore_global
+ln -sf $DIR/.vimrc ~/.vimrc
 
