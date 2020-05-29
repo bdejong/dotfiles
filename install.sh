5477be3e8a5fbfbeeaded6c43dfb51165b87c57f
Bram de Jong <bram.dejong@gmail.com>
2020-05-29 19:12:12 +0200

    Switched to zsh

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ln -sf $DIR/bash_profile ~/.bash_profile
-ln -sf $DIR/bash_prompt ~/.bash_prompt
 ln -sf $DIR/brewpackages ~/.brewpackages
 ln -sf $DIR/gitconfig ~/.gitconfig
 ln -sf $DIR/gitignore_global ~/.gitignore_global
 ln -sf $DIR/vimrc ~/.vimrc
-ln -sf $DIR/liquidpromptrc ~/.liquidpromptrc
 ln -sf $DIR/pylintrc ~/.pylintrc
+ln -sf $DIR/zshrc ~/.zshrc
